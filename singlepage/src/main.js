e03a244b6c6f3cd26d27183630549e3c8815b401
= <alfred@stensatter.se>
2023-11-23 01:36:14 +0100

    update

---
+++ singlepage/src/main.js
@@ -0,0 +1,30 @@
+
+let prefix = "PAGE-"
+
+function turnOffAll(){
+	const pages = document.querySelectorAll("."+prefix);
+	for (const page of pages) {
+		console.log(page)
+		setPageOff(page)
+	}
+}
+
+function setPageOff(element){
+	if (element.style.display === "block") {
+	  element.style.display = "none";
+	}
+}
+
+function setPageOn(element){
+	turnOffAll()
+	if (element.style.display === "none") {
+	  element.style.display = "block";
+	}
+}
+
+function changePage(id){
+	let element = document.getElementById(prefix+id)
+	setPageOn(element);
+}
+
+
